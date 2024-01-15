eb63fece7244e0639acaee65e982fd325a2f9f99
Sandhyarani <sanvyrani@gmail.com>
2024-01-15 16:57:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+
+import './App.css'
+
+import { Route,RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './Components/Home/Home'
+import About from './Components/About/About'
+import Contact from './Components/Contact/Contact'
+import User from './Components/User/User'
+import Github, {githubInfoLoader} from './Components/Github/Github'
+
+
+function App() {
+
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+     <Route path='/' element={<Layout/>}>
+        <Route path='' element={<Home/>}/>
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route 
+      loader={githubInfoLoader}
+      path='github' 
+      element={<Github />}
+       />
+     </Route>
+      
+    )
+  )
+
+  return (
+   <>
+    {/* <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Header/>}/>
+      </Routes>
+    </BrowserRouter> */}
+    <RouterProvider router={router}/>
+    
+   </>
+  )
+}
+
+export default App
